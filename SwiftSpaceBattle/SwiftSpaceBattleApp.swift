08699bb219183baaf2348411c9da497297c07d44
Julio César Fernández <juliocesar.fernandez@gabhel.com>
2025-01-04 19:18:54 +0100

    Initial Commit

---
+++ SwiftSpaceBattle/SwiftSpaceBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpaceBattleApp.swift
+//  SwiftSpaceBattle
+//
+//  Created by Julio César Fernández Muñoz on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpaceBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
